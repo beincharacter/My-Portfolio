bea9b8664f769930bda9c9ae51fe2f7790468071
shubham pal <shubhampalatwork@gmail.com>
2023-02-25 11:52:01 +0530

    meet update

--- src/Componant/About/details.jsx
+++ src/Componant/About/details.jsx
@@ -1,28 +1,42 @@
-import React from 'react';
-import './about.scss';
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { FaHtml5, FaCss3Alt, FaSass, FaJsSquare, FaReact, FaNodeJs, FaDatabase, FaGitAlt } from "react-icons/fa";
+import './SkillCard.scss';
 
-const Details = () => {
-    return (
-        <div className='details__card'>
-            <table style={{border: "1px solid black", bordeCollapse: "collapse"}}>
-                <tbody>
-                    <tr>
-                        <td style={{border: "1px solid black", padding: "8px"}}>Name</td>
-                        <td style={{border: "1px solid black", padding: "8px"}}>Shubham</td>
-                    </tr>
-                    <tr>
-                        <td style={{border: "1px solid black", padding: "8px"}}>email</td>
-                        <td style={{border: "1px solid black", padding: "8px"}}>shubhampalatwork@gmail.com</td>
-                    </tr>
-                    <tr>
-                        <td style={{border: "1px solid black", padding: "8px"}}>Located</td>
-                        <td style={{border: "1px solid black", padding: "8px"}}>Ludhiana, Punjab</td>
-                    </tr>
-                </tbody>
-            </table>
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
 
-        </div>
-    )
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
+
+  const skills = [
+    { name: 'HTML5', icon: <FaHtml5 />, description: 'HyperText Markup Language' },
+    { name: 'CSS3', icon: <FaCss3Alt />, description: 'Cascading Style Sheets' },
+    { name: 'SCSS', icon: <FaSass />, description: 'Sass (Syntactically Awesome Style Sheets)' },
+    { name: 'JavaScript', icon: <FaJsSquare />, description: 'A high-level programming language' },
+    { name: 'React', icon: <FaReact />, description: 'A JavaScript library for building user interfaces' },
+    { name: 'Node.js', icon: <FaNodeJs />, description: 'A JavaScript runtime built on Chrome\'s V8 JavaScript engine' },
+    { name: 'MongoDB', icon: <FaDatabase />, description: 'A cross-platform document-oriented database program' },
+    { name: 'Git', icon: <FaGitAlt />, description: 'A free and open source distributed version control system' }
+  ];
+
+  return (
+      <div className='card-container'>
+        {skills.map((skill, i) => (
+          <div className='skill-box'  key={i}>
+            <div className="skill-card" title={skill.description}>
+              <div className="skill-card__icon">
+                {skill.icon}
+              </div>
+              <div className="skill-card__name">
+                <h3>{skill.name}</h3>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+  );
 }
 
-export default Details
+export default ControlledCarousel;
